671f2f159fb729468896fad8f634c04f23316234
Pasha <p-a-shok@mail.ru>
2017-11-20 21:53:07 +0300

    delete basketServlet

--- src/ru/mail/pashok/shop/controller/DeleteOrderServlet.java
+++ src/ru/mail/pashok/shop/controller/DeleteOrderServlet.java
@@ -1,10 +1,7 @@
 package ru.mail.pashok.shop.controller;
 
-import ru.mail.pashok.shop.repository.model.Order;
 import ru.mail.pashok.shop.service.OrderServiceImpl;
-import ru.mail.pashok.shop.service.UserServiceImpl;
 import ru.mail.pashok.shop.service.model.OrderDTO;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +10,6 @@ import java.io.IOException;
 import java.util.List;
 
 public class DeleteOrderServlet extends HttpServlet {
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Integer id = Integer.valueOf(request.getParameter("id"));
         List<OrderDTO> basket = (List<OrderDTO>) request.getSession().getAttribute("basket");
